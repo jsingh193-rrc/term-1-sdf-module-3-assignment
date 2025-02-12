ba7c5133db3cec6dfd4b11144b033766427e4186
Jashandeep Singh <jsingh193@academic.rrc.ca>
2025-02-12 14:59:30 -0600

    Add interest calculator according to the users' balance.

--- interest_update.py
+++ interest_update.py
@@ -14,8 +14,27 @@ file = open("account_balances.txt", "r")
 
 with open('account_balances.txt', 'r') as file:
     reader = csv.DictReader(file)
-    for line in file:
-        account_number, balance = line.strip().split('|')
-        customer_balances[account_number] = float(balance)
+    with open("account_balances.txt", "r") as file:
+        for line in file:
+            account_number, balance = line.strip().split("|")
+            customer_balances[account_number] = float(balance)
 
 pprint.pprint(customer_balances)
+
+interest_earned = {}
+
+for account, balance in customer_balances.items():
+    if balance < 0:
+        interest_rate = 0.10
+    elif balance < 1000:
+        interest_rate = 0.01
+    elif balance < 5000:
+        interest_rate = 0.025
+    else:
+        interest_rate = 0.05
+
+    interest_amount = (balance * interest_rate) / 12
+
+    interest_earned[account] = interest_rate
+
+pprint.pprint(interest_earned)
