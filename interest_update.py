a9681966773fb3b95271d7a46a996ec828ba55f8
Jashandeep Singh <jsingh193@academic.rrc.ca>
2025-02-12 13:21:52 -0600

    Add the account_balances data to the Dictionary and print it to console.

--- interest_update.py
+++ interest_update.py
@@ -2,3 +2,20 @@
 
 __author__ = "Jashandeep Singh"
 __version__ = "1.0.0"
+
+import csv
+import pprint
+
+customer_balances = {
+
+}
+
+file = open("account_balances.txt", "r")
+
+with open('account_balances.txt', 'r') as file:
+    reader = csv.DictReader(file)
+    for line in file:
+        account_number, balance = line.strip().split('|')
+        customer_balances[account_number] = float(balance)
+
+pprint.pprint(customer_balances)
